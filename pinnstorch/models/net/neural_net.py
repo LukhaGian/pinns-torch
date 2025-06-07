b54661fdf9327005f60d766c45f4249033101c47
Gianmaria Lukha-Lucca <98236804+LukhaGian@users.noreply.github.com>
2025-06-07 11:57:29 +0200

    Update neural_net.py
    Removed normalization for epsilon network input

--- pinnstorch/models/net/neural_net.py
+++ pinnstorch/models/net/neural_net.py
@@ -273,9 +273,9 @@ class ParallelNet(nn.Module): # rho, j and epsilon Network
         else:
             x, y, z = spatial
             z1 = torch.cat((x, y, z, time), 1)
-            z2 = torch.cat((x, y), 1)
+            z2 = torch.cat((x, y, z), 1)
         z1 = 2.0 * (z1 - self.lb) / (self.ub - self.lb) - 1.0  
-        z2 = 2.0 * (z2 - self.lb) / (self.ub - self.lb) - 1.0
+        #z2 = 2.0 * (z2 - self.lb) / (self.ub - self.lb) - 1.0
 
         z1 = self.model1(z1)
         z2 = torch.exp(self.model2(z2)) # enforcing positivity
